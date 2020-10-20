b3a37ca71ccfda00e4740876afef1ee88faf9c51
Steven Hemmy <shemmy@imagescape.com>
2020-10-20 11:53:00 -0500

    limit feedparser dependency to <6.0.0

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     install_requires=[
         'six',
         'requests>=2.7.0',
-        'feedparser>=5.1.3',
+        'feedparser>=5.1.3,<6.0.0',
     ],
     include_package_data=True,
     zip_safe=False
